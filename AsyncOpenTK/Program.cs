994fb4233942341414017626e9b5385a64679fc8
Christoph Müller <chrism@g-graphix.de>
2020-05-19 21:20:56 +0200

    Added FuseeApp

--- AsyncOpenTK/Program.cs
+++ AsyncOpenTK/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.ComponentModel;
-using OpenToolkit.Graphics.GL;
+using System.Threading;
+using System.Threading.Tasks;
 using OpenToolkit.Windowing.Common;
 using OpenToolkit.Windowing.Desktop;
 // using OpenTK.Graphics;
@@ -19,168 +19,145 @@ namespace AsyncOpenTK
 
     public class MyGameWindow : GameWindow
     {
+        protected FuseeApp fuseeApp;
+
         public MyGameWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
         {
-        }
+            fuseeApp = new FuseeApp();
 
-        public override void Close()
-        {
-            base.Close();
-        }
-
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-
-        public override void ProcessEvents()
-        {
-            base.ProcessEvents();
-        }
-
-        public override void Run()
-        {
-            base.Run();
-        }
-
-        public override void SwapBuffers()
-        {
-            base.SwapBuffers();
-        }
-
-        public override string ToString()
-        {
-            return base.ToString();
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
-        }
-
-        protected override void OnClosed()
-        {
-            base.OnClosed();
-        }
-
-        protected override void OnClosing(CancelEventArgs e)
-        {
-            base.OnClosing(e);
-        }
-
-        protected override void OnFileDrop(FileDropEventArgs e)
-        {
-            base.OnFileDrop(e);
-        }
-
-        protected override void OnFocusedChanged(FocusedChangedEventArgs e)
-        {
-            base.OnFocusedChanged(e);
-        }
-
-        protected override void OnJoystickConnected(JoystickEventArgs e)
-        {
-            base.OnJoystickConnected(e);
-        }
-
-        protected override void OnKeyDown(KeyboardKeyEventArgs e)
-        {
-            base.OnKeyDown(e);
-        }
-
-        protected override void OnKeyUp(KeyboardKeyEventArgs e)
-        {
-            base.OnKeyUp(e);
         }
 
         protected override void OnLoad()
         {
             base.OnLoad();
+            fuseeApp.Init();
         }
 
-        protected override void OnMinimized(MinimizedEventArgs e)
-        {
-            base.OnMinimized(e);
-        }
-
-        protected override void OnMonitorConnected(MonitorEventArgs e)
-        {
-            base.OnMonitorConnected(e);
-        }
-
-        protected override void OnMouseDown(MouseButtonEventArgs e)
-        {
-            base.OnMouseDown(e);
-        }
-
-        protected override void OnMouseEnter()
-        {
-            base.OnMouseEnter();
-        }
-
-        protected override void OnMouseLeave()
-        {
-            base.OnMouseLeave();
-        }
-
-        protected override void OnMouseMove(MouseMoveEventArgs e)
-        {
-            base.OnMouseMove(e);
-        }
-
-        protected override void OnMouseUp(MouseButtonEventArgs e)
-        {
-            base.OnMouseUp(e);
-        }
-
-        protected override void OnMouseWheel(MouseWheelEventArgs e)
-        {
-            base.OnMouseWheel(e);
-        }
-
-        protected override void OnMove(WindowPositionEventArgs e)
-        {
-            base.OnMove(e);
-        }
-
-        protected override void OnRefresh()
-        {
-            base.OnRefresh();
-        }
 
         protected override void OnRenderFrame(FrameEventArgs args)
         {
             base.OnRenderFrame(args);
+            fuseeApp.RenderAFrame();
         }
 
         protected override void OnRenderThreadStarted()
         {
             base.OnRenderThreadStarted();
         }
+    }
 
-        protected override void OnResize(ResizeEventArgs e)
+
+    public class FuseeApp
+    {
+        protected string res;
+
+        protected async Task LoadAssets()
         {
-            base.OnResize(e);
+            Console.WriteLine($"About to load asset. In Thread: {Thread.CurrentThread.ManagedThreadId}");
+            var asset = await AssetStorage.GetStringAsync("honz");
+            
+            // Do something with the asset right here
+            Console.WriteLine($"Asset is present. In Thread: {Thread.CurrentThread.ManagedThreadId}");
+            res = asset;
         }
 
-        protected override void OnTextInput(TextInputEventArgs e)
+        public void Init()
         {
-            base.OnTextInput(e);
+            // Fire-and-forget call to LoadAssets!!! NO await!!!
+            LoadAssets();
+            // AsyncContext.Run(LoadAssets);
+            Console.WriteLine("End Of Init");
         }
 
-        protected override void OnUnload()
+        public void RenderAFrame()
         {
-            base.OnUnload();
+       
         }
 
-        protected override void OnUpdateFrame(FrameEventArgs args)
+        public bool Stop {get; set;}
+
+        //public void Run()
+        //{
+        //    Init();
+        //    while (!Stop)
+        //    {
+        //        // PerformPendingPostAwaits();
+        //        RenderAFrame();
+        //    }
+        //}
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    //
+    // Framework AssetLoader. Should hide await-calls and async creeping from calling instances
+
+    public class AssetStorage
+    {
+        public static async Task<string> GetStringAsync(string id)
         {
-            base.OnUpdateFrame(args);
+            string result;
+            using (var stream = new AsyncStream(id))
+            {
+                result = await stream.GetContentsAsync();
+            }
+            return result;
         }
     }
+
+    ////////////////////////////////////////////////////////////////////////
+    //
+    // Standard-Lib async file loader. Called by the AssetLoader
+
+    public class AsyncStream : IDisposable
+    {
+        private bool disposedValue;
+
+        public AsyncStream(string streamID)
+        {
+
+        }
+
+        public async Task<string> GetContentsAsync()
+        {
+            return await Task<string>.Run(() => 
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(200);
+                    System.Console.WriteLine(".");
+                }
+                return "This is the Stream Contents";
+            });
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: Verwalteten Zustand (verwaltete Objekte) bereinigen
+                }
+
+                // TODO: Nicht verwaltete Ressourcen (nicht verwaltete Objekte) freigeben und Finalizer überschreiben
+                // TODO: Große Felder auf NULL setzen
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: Finalizer nur überschreiben, wenn "Dispose(bool disposing)" Code für die Freigabe nicht verwalteter Ressourcen enthält
+        // ~AsyncStream()
+        // {
+        //     // Ändern Sie diesen Code nicht. Fügen Sie Bereinigungscode in der Methode "Dispose(bool disposing)" ein.
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Ändern Sie diesen Code nicht. Fügen Sie Bereinigungscode in der Methode "Dispose(bool disposing)" ein.
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
 }
